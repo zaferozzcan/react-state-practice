494d46c25bc43372c926ae5df2708f9e5e9a628e
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-11-09 14:50:55 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Receipts from "./components/Receipts";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Receipts />
+    </div>
+  );
+}
